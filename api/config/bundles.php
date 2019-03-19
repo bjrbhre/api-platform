cbed3f411a9e0d8a145927f7c5f26b374acead5b
Pierre Beauhaire <bjr@bhre.me>
2019-03-19 07:39:21 +0100

    Requirements for JWT setup
    Mostly inspired from [doc](https://api-platform.com/docs/core/jwt/).
    - use Lexik for JWT authentication
    - use FOSUserBundle for User entity
    - FOSUserBundle config depends on SwiftMailer
    - add doctrine fixtures to provision users
    
    ```
    docker-compose exec php composer require "lexik/jwt-authentication-bundle"
    docker-compose exec php composer require friendsofsymfony/user-bundle
    docker-compose exec php composer require swiftmailer-bundle
    docker-compose exec php composer require --dev doctrine/doctrine-fixtures-bundle
    ```

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,4 +11,8 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
