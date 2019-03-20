6a9056ec71f35a7526561bc9e187ecf8b895a0b6
Pierre Beauhaire <bjr@bhre.me>
2019-03-20 16:49:42 +0100

    /users/me

--- api/src/EventSubscriber/UserSubscriber.php
+++ api/src/EventSubscriber/UserSubscriber.php
@@ -2,20 +2,47 @@
 
 namespace App\EventSubscriber;
 
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
 
 class UserSubscriber implements EventSubscriberInterface
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function onKernelRequest(GetResponseEvent $event)
     {
-        // ...
+        $request = $event->getRequest();
+
+        if ('api_users_get_item' !== $request->attributes->get('_route')) {
+            return;
+        }
+
+        if ('me' !== $request->attributes->get('id')) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $request->attributes->set('id', $user->getId());
     }
 
     public static function getSubscribedEvents()
     {
         return [
-           'kernel.request' => 'onKernelRequest',
+            'kernel.request' => ['onKernelRequest', EventPriorities::PRE_READ]
         ];
     }
 }
